d6641a67b2325a042f6bad2a7b25f973cddd5644
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-05-08 16:07:36 -0300

    ajuste de script

--- script.js
+++ script.js
@@ -5,27 +5,29 @@ const butEnviar = document.getElementById('but-enviar');
 const mes = document.getElementById('imes');
 const anos = document.getElementById('ianos');
 let nome = document.getElementById('inome');
-let senha = document.getElementById('inum').value.length;
-let cardnum = document.getElementById('cardnum');
-cardnum.innerHTML = senha
+let senha = document.getElementById('inum');
 butEnviar.addEventListener('click',(e)=>{
     e.preventDefault();
     if(nome.value.length < 5 || nome.value.length > 25){
-    alert('Nombre no corresponde, informe los datos correntamente');
-}else if (senha.value.length < 16){
-    alert('numero de la tarjeta no corresponde, informe los datos correntamente')
-}else if(mes.value.length == 0 || anos.value.length == 0){
-    alert('Informe os datos correntamente.')
-
-}else{
-    form.style.display = 'none'
-    posConfirm.style.display = 'block';
-}
-})
+        nome.style.border = '1px solid red'
+       alert('Nombre no corresponde, informe los datos correntamente');
+        }else if (senha.value.length < 16){
+            senha.style.border = '1px solid red'
+            alert('numero de la tarjeta no corresponde, informe los datos correntamente');
+        }else if(mes.value.length == 0 || anos.value.length == 0){
+            mes.style.border = '1px solid red'
+             alert('Informe los datos de validad correntamente.');
+        }else{
+            form.style.display = 'none';
+            posConfirm.style.display = 'block';
+        }
+    }
+)
 
 posConfirm.addEventListener('click',(e)=>{
     e.preventDefault();
-    form.style.display = 'block'
-    posConfirm.style.display = 'none'
+    form.style.display = 'block';
+    posConfirm.style.display = 'none';
 
 })
+
