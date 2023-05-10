728dab7c9ac421d4c1f78c480290338e8be35b9e
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-05-10 16:02:53 +0200

    ajustes no modo desktop

--- script.js
+++ script.js
@@ -12,12 +12,15 @@ butEnviar.addEventListener('click',(e)=>{
         nome.style.border = '1px solid red'
        alert('Nombre no corresponde, informe los datos correntamente');
         }else if (senha.value.length < 16){
+            nome.style.border = 'black'
             senha.style.border = '1px solid red'
             alert('numero de la tarjeta no corresponde, informe los datos correntamente');
         }else if(mes.value.length == 0 || anos.value.length == 0){
+            senha.style.border = 'black'
             mes.style.border = '1px solid red'
              alert('Informe los datos de validad correntamente.');
         }else{
+            mes.style.border = 'black';
             form.style.display = 'none';
             posConfirm.style.display = 'block';
         }
